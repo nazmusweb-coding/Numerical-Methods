5f9278d90a9ea9cbf364063c98228be3069849d9
nazmusweb-coding <nazmusweb.coding@gmail.com>
2024-09-15 13:14:33 +0600

    added using std lines

--- Simple Iteration/simple_iteration_method.cpp
+++ Simple Iteration/simple_iteration_method.cpp
@@ -2,14 +2,20 @@
 #include <iomanip>
 #include <cmath>
 
+using std::cout;
+using std::cin;
+using std::endl;
+using std::fixed;
+using std::setprecision;
+
 //-----------------------------------Function Declaration----------------------------------
 void simple_iteration_method(double X)
 {
     int iteration = 1;                                  // Initialize the iteration counter
     while (true)
     {
-        std::cout << std::fixed << std::setprecision(16) 
-                << iteration << ' ' << X << std::endl;  // Print the current iteration number and X value
+        cout << fixed << setprecision(5) 
+             << iteration << ' ' << X << endl;          // Print the current iteration number and X value
         double Xnew = (2 * pow(X, 2) + 3) / 5;          // Compute the new value of X using the iteration formula
         if (X == Xnew)                                  // Check if the current value X is the same as the new value Xnew
         {
@@ -18,15 +24,15 @@ void simple_iteration_method(double X)
         X = Xnew;                                       // Update X with the new value
         iteration++;                                    // Increment the iteration counter
     }
-    std::cout << "Root found at iteration " << iteration << ", root is " << X << std::endl;  // Print the final result
+    cout << "Root found at iteration " << iteration << ", root is " << X << endl;  // Print the final result
 }
 
 //-----------------------------------Main Program-------------------------------------------
 int main(int argc, char **argv)
 {
     double X;
-    std::cout << "Enter value for X: ";                 // Prompt user for the initial value of X
-    std::cin >> X;                                      // Read the value of X from standard input
+    cout << "Enter value for X: ";                 // Prompt user for the initial value of X
+    cin >> X;                                      // Read the value of X from standard input
     simple_iteration_method(X);                         // Call the iteration method with the initial value
 
     return 0;
